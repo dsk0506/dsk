e44c723eb1b62af36258ead5503841d2236aa1f9
dsk0506 <dsk0506@163.com>
2014-11-05 16:31:29 +0800

    test2

---
+++ function.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2014/11/5
+ * Time: 16:30
+ */
+function test(){
+    echo "test";
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2014/11/5
+ * Time: 16:30
+ */
+include 'function.php';
+test();
